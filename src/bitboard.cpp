28f8e3271d471413c4e9d4d8eefc493cd6a3e39e
Ricardo <ricardo.chewzhenwei@gmail.com>
2026-02-10 23:03:13 +0800

    Don't know

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -1,2 +1,12 @@
 #include "defs.h"
 
+//References:
+//https://www.chessprogramming.org/Square_Mapping_Considerations#LittleEndianRankFileMapping
+//https://www.youtube.com/watch?v=3uBCUF_qHcg&list=PLZ1QII7yudbc-Ky058TEaOstZHVbT-2hg&index=4
+
+//Setting up board representation (120 square and 64 square)
+//Assigned Coordinate to the board (e.g. a1, h7)
+
+//TODO: Setting up bitboard 120 squares
+
+//TODO: Setting up bitboard 64 squares
